4fba0bdb906fe2792e483e3c4f0728f6cedfc84e
adriamdevs <murigucharles3@gmail.com>
2025-11-20 14:46:13 +0300

    Mod: Add tooling functionality

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # A Custom research agent
 
-This is a simple research agent
+My simple research assistant agent.
 
-## To run
+## To clone & run this project
+
+`` git clone <repo>
+`` python -m venv\venv
+`` pip install -r requirements.txt
+`` get your API key from your LLM provider of choice(OpenAI or anthropic)
+
+Feel free to tweak to according to your preference.
--- main.py
+++ main.py
@@ -1,12 +1,67 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_openai import ChatOpenAI
+#from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
 
-load_dotenv()
+load_dotenv() # load the api key from the .env file
+
+#llm_two = ChatOpenAI(model="gpt-40-mini") -> You could go with OpenAI if you like
+
+#test_response = llm_One.invoke("What is AI?") ->test the LLM
+
+## Prompt Template
+class Research_resp(BaseModel):
+    topic : str
+    summary : str
+    sources: list[str]
+    tools_used: list[str]
 
 # LLM Setup
 llm_one = ChatAnthropic(model = "claude-3-5-sonnet-20241022")
-#llm_two = ChatOpenAI(model="gpt-40-mini")
 
-response = llm.invoke("What is AI?")
+# Take LLM output & parse it into the model to use as a normal python object
+parser = PydanticOutputParser(pydantic_object = Research_resp)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+        "system",
+          """ You are a research assistant that will help generate a research paper.
+              Answer the user query and use necessary tools.
+              Wrap the output in this format and provide no other text \n {format_instructions}
+          """
+        ),
+        ("placeholder", "{chat_history}"),
+        "human", "{query}",
+        "placeholder", "{agent_scratchpad}",
+    ]
+).partial(format_instructions = parser.get_format_instructions())
+
+tools = [search_tool, wiki_tool, save_tool] # feel free to add more tools
+
+# Create the agent
+agent = create_tool_calling_agent(
+    llm = llm_one,
+    prompt = prompt,
+    tools = []
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can I help you research")
+raw_response = agent_executor.invoke({"query" : query})
+
+#print(raw_response)
+
+# Structure the output -> get text from the first value of the output & parse it into a python dictionary
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response:",e,"Raw response-",raw_response)
+
+
+# print(structured_response.topic)
